ccfee90f598fe74ec57840e7ba95af84b9cdeb2d
FanFanFan1997 <34194406+FanFanFan1997@users.noreply.github.com>
2017-12-02 22:07:37 -0500

    Add files via upload

---
+++ BasicBot.py
@@ -0,0 +1,31 @@
+# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+client = Bot(description="", command_prefix="-", pm_help = True)
+
+# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
+# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+
+
+# This is a basic example of a call and response command. You tell it do "this" and it does it.
+@client.command()
+async def fuck(*args):
+	await client.say("brandon ugly shit!")
+
+
+# After you have modified the code, feel free to delete the line above (line 33) so it does not keep popping up everytime you initiate the ping commmand.
+	
+client.run('Mzg2NDA4NzMyOTgxOTg1Mjgy.DQPtCA.ROqcrIa7sJuESunIPgqDpB2OpVo')
+
+
+
+# The help command is currently set to be Direct Messaged.
+# If you would like to change that, change "pm_help = True" to "pm_help = False" on line 9.
